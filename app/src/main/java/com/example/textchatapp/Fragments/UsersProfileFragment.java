902f4b5be4121a8000b3de0071ef8a40570f7812
DEEPALI SINGH <deepalisingh15899@gmail.com>
2021-07-27 18:12:43 +0530

    updated code and design for profile fragment

--- app/src/main/java/com/example/textchatapp/Fragments/UsersProfileFragment.java
+++ app/src/main/java/com/example/textchatapp/Fragments/UsersProfileFragment.java
@@ -2,24 +2,73 @@ package com.example.textchatapp.Fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.example.textchatapp.MainActivity;
+import com.example.textchatapp.Model.User;
 import com.example.textchatapp.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 
 public class UsersProfileFragment extends Fragment {
 
 
+    CircleImageView profileImageView;
+    TextView usernameText;
+
+    FirebaseUser firebaseUser;
+    DatabaseReference dbReference;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
+        View view = inflater.inflate(R.layout.fragment_users_profile, container,false);
 
-        return inflater.inflate(R.layout.fragment_users_profile, container, false);
+        profileImageView = view.findViewById(R.id.profileImage_userProfile);
+        usernameText = view.findViewById(R.id.userNameText_profile);
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        dbReference = FirebaseDatabase.getInstance().getReference("Users").child(firebaseUser.getUid());
+
+        dbReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                User user = snapshot.getValue(User.class);
+                assert user != null;
+                usernameText.setText(user.getUsername());
+
+                if (user.getImageURL().equals("default"))
+                    profileImageView.setImageResource(R.drawable.default_profile_image);
+                else
+                    Glide.with(getContext()).load(user.getImageURL()).into(profileImageView);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        return view;
     }
-
 
 }
